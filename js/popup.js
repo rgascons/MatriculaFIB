ed45412f944a956cba53c9d6bc5554219872cb1b
ricard <gascons1995@gmail.com>
2015-06-23 01:14:53 +0200

    Grup input field and some other tweaks

--- js/popup.js
+++ js/popup.js
@@ -8,34 +8,57 @@ $(document).ready(function(){
      	return false;
    	});
    	var i=0;
+
    	var addNewRow = (function() {
    		var inputVal = $("#inputAssig").val().toUpperCase();
-   		if (inputVal.length > 0) {
-   			var placesLliures = 3;
-   			var placesTotals = 40;
-   			$('#assig'+i).html("<td>"+ (i+1) +
-      		"<td>"+inputVal+"</td><td>"+placesLliures+"</td><td>"+placesTotals+"</td>");
-    		//Avoiding duplicate IDs
-    		if (!document.getElementById('assig' + (i+1)))
-      			$('#subjectsTable').append('<tr id="assig'+(i+1)+'"></tr>');
-      			
-      		i++; 
-   		} else {
+   		var grupVal = $("#inputGrup").val();
+   		if (inputVal.length <= 0) {
    			toastr.options = {
    				"newestOnTop": true,
    				"positionClass": "toast-bottom-center"
    			}
       		toastr.error("El camp d'entrada està buit");
-
-      	}
+      		return false;
+   		} else if (grupVal < 20 || grupVal > 99) {
+   			toastr.options = {
+   				"newestOnTop": true,
+   				"positionClass": "toast-bottom-center"
+   			}
+      		toastr.error("Grup conté un valor incorrecte");
+      		return false;
+   		} else {
+   			var placesLliures = 3;
+   			var placesTotals = 40;
+   			$('#assig'+i).html("<td>"+ (i+1) +
+      		"<td>"+inputVal+"</td><td>"+grupVal+"</td><td>"+placesLliures+"</td><td>"+placesTotals+"</td>");
+    		//Avoiding duplicate IDs
+    		if (!document.getElementById('assig' + (i+1)))
+      			$('#subjectsTable').append('<tr id="assig'+(i+1)+'"></tr>');
+      			
+      		i++; 
+   		}
+   		return true;
    	});
    	//Add new row to the table
-   	$("#addBtn").click(addNewRow);
-  	$("#assigForm").submit(function() {
-  		addNewRow();
-  		$("#inputAssig").val("");
+   	$("#addBtn").click(function() {
+   		if (addNewRow()) {
+   			$("#inputAssig").val("");
+   			$("#inputGrup").val("");
+   		}
   		return false;
-  	});
+   	});
+  	$("#assigForm").each(function() {
+        $(this).find('input').keypress(function(e) {
+            if(e.which == 10 || e.which == 13) {
+                if (addNewRow()) {
+   					$("#inputAssig").val("");
+   					$("#inputGrup").val("");
+   				}
+  				return false;
+            }
+        });
+        $(this).find('input[type=submit]').hide();
+    });
   	//Delete last row
   	$("#delBtn").click(function(){
     	if(i>0){
